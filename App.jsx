d1e816c49fa39bc5b1ae851f452937adc6491a2d
이진우 <dlwlsdn8864@naver.com>
2024-08-30 15:29:36 +0900

    router로 인한 배치 이상문제 해결 page -> route_page사용으로 해결
    js -> jsx파일로 변경
    expo에 맞게 수정

---
+++ App.jsx
@@ -0,0 +1,26 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage.jsx'
+import MoneyChange from './pages/MoneyChange.jsx'
+import Header from './components/MenuBarHeader.jsx'
+import AccountList from './pages/AccountList.jsx'
+import PayChart from './pages/PayChart.jsx'
+
+function App() {
+  return (
+    <Router>
+      <Header/>
+      <div style={{ marginTop: '37px' }}></div> {/* Header 높이만큼의 마진 추가 */}
+      <div className='route_page'>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/moneyChange' element={<MoneyChange />} />
+          <Route path='/accountlist' element={<AccountList />} />
+          <Route path='/paychart' element={<PayChart />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
