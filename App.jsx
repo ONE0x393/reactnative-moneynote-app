35aaa60af9a60c49ed86e427f12529529d94e11d
이진우 <dlwlsdn8864@naver.com>
2024-09-06 02:41:53 +0900

    기존 app.jsx 주석처리

--- App.jsx
+++ App.jsx
@@ -51,3 +51,43 @@ const styles = StyleSheet.create({
 });
 
 export default App;
+
+
+/*
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage.jsx';
+import MoneyChange from './pages/MoneyChange.jsx';
+import Header from './components/MenuBarHeader.jsx';
+import AccountList from './pages/AccountList.jsx';
+import PayChart from './pages/PayChart.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import AccountForm from './pages/AccountForm.jsx';
+import AccountEditPage from './pages/AccountEditPage.jsx';
+import TermsPage from './pages/TermsPage.jsx';
+import SignupPage from './pages/SignupPage.jsx';
+
+function App() {
+  return (
+    <Router>
+      <Header/>
+      <div style={{ marginTop: '37px' }}></div> 
+      <div className='route_page'>
+        <Routes>
+          <Route path='/' element={<LoginPage />} />
+          <Route path="/terms" element={<TermsPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path='/home' element={<HomePage />} />
+          <Route path='/moneyChange' element={<MoneyChange />} />
+          <Route path='/accountlist' element={<AccountList />} />
+          <Route path='/paychart' element={<PayChart />} />
+          <Route path='/accountform' element={<AccountForm />} />
+          <Route path='/accountedit/:accountId' element={<AccountEditPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+*/
